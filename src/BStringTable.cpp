a3cdd02079dd638388bbf2de5bc5cf6a0024ee3b
Einar Johan Trøan Sømåen <einarjohants@gmail.com>
2012-03-05 01:17:47 +0100

    WINTERMUTE: Make BStringTable independent of dcgf.h

--- src/BStringTable.cpp
+++ src/BStringTable.cpp
@@ -23,7 +23,9 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
-#include "dcgf.h"
+#include "PlatformSDL.h"
+#include "BFileManager.h"
+#include "BGame.h"
 #include "BStringTable.h"
 #include "StringUtil.h"
 
