fbeb4211d3f18bd5203bdf88416a42735d0ff9ce
Einar Johan Trøan Sømåen <einarjohants@gmail.com>
2012-03-05 02:21:46 +0100

    WINTERMUTE: Make BViewPort independent of dcgf

--- src/BViewport.cpp
+++ src/BViewport.cpp
@@ -23,7 +23,8 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
-#include "dcgf.h"
+#include "BGame.h"
+#include "PlatformSDL.h"
 #include "BViewport.h"
 
 namespace WinterMute {
