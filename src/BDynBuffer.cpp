55cb1b32ac0a15d293afe13e41ed08c33953fb85
Einar Johan Trøan Sømåen <einarjohants@gmail.com>
2012-03-05 01:16:36 +0100

    WINTERMUTE: Make BDynBuffer independent of dcgf.h

--- src/BDynBuffer.cpp
+++ src/BDynBuffer.cpp
@@ -23,7 +23,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
-#include "dcgf.h"
+#include "BGame.h"
 #include "BDynBuffer.h"
 
 namespace WinterMute {
